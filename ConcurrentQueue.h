766628beb3483f186dcc05c90648580d5c366026
Chenna Reddy <chenna.reddy.alavala@ing.nl>
2017-07-03 03:48:20 +0200

    using unsigned

--- ConcurrentQueue.h
+++ ConcurrentQueue.h
@@ -24,20 +24,20 @@ private:
         return (n <= 1) ? p : Log2(n / 2, p + 1);
     }
 
-    static constexpr uint64_t closestExponentOf2(int64_t x) {
-        return (1UL << ((int64_t) (Log2(SIZE - 1)) + 1));
+    static constexpr uint64_t closestExponentOf2(uint64_t x) {
+        return (1UL << ((uint64_t) (Log2(SIZE - 1)) + 1));
     }
 
     static constexpr uint64_t mRingModMask = closestExponentOf2(SIZE) - 1;
     static constexpr uint64_t mSize = closestExponentOf2(SIZE);
 
     T mMem[mSize];
-    std::atomic_int_fast64_t mWriteSlotPtr;
-    std::atomic_int_fast64_t mReadPtr;
-    std::atomic_int_fast64_t mWritePtr;
+    std::atomic_uint_fast64_t mWriteSlotPtr;
+    std::atomic_uint_fast64_t mReadPtr;
+    std::atomic_uint_fast64_t mWritePtr;
 
 public:
-    ConcurrentQueue() : mWritePtr(0), mWriteSlotPtr(-1), mReadPtr(0) {}
+    ConcurrentQueue() : mWritePtr(1), mWriteSlotPtr(0), mReadPtr(1) {}
 
     /**
      * Pop an element from Queue
@@ -49,7 +49,7 @@ public:
         if (!peek()) {
             return false;
         }
-        int64_t readPtr = mReadPtr;
+        uint64_t readPtr = mReadPtr;
         // Backup the element
         ret = mMem[readPtr & mRingModMask];
         // Let the Producers know that we are done with this position
@@ -69,9 +69,9 @@ public:
      * Get the number of elements in the Queue
      * @return Number of elements in Queue
      */
-    int64_t getCount() const {
-        int64_t readPtr = mReadPtr;
-        int64_t writePtr = mWritePtr;
+    uint64_t getCount() const {
+        uint64_t readPtr = mReadPtr;
+        uint64_t writePtr = mWritePtr;
         return writePtr > readPtr ? writePtr - readPtr : readPtr - writePtr;
     }
 
@@ -82,13 +82,13 @@ public:
      */
     void push(const T &pItem) {
         for (int i = 0; i < MAX_SPIN_ON_BUSY; ++i) {
-            int64_t readPtr = mReadPtr;
-            int64_t writePtr = mWritePtr;
-            int64_t  size = (writePtr > readPtr ? writePtr - readPtr : readPtr - writePtr);
+            uint64_t readPtr = mReadPtr;
+            uint64_t writePtr = mWritePtr;
+            uint64_t  size = (writePtr > readPtr ? writePtr - readPtr : readPtr - writePtr);
             if (size >= mSize) {
                 continue;
             }
-            int64_t writeSlotPtr = writePtr - 1;
+            uint64_t writeSlotPtr = writePtr - 1;
             // Get write slot
             if (mWriteSlotPtr.compare_exchange_strong(writeSlotPtr, writePtr)) {
                 // This thread got the slot, fill the element
